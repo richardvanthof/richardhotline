b49e84f371fce9a0ee6a9c35efb8e875d1d6605e
RichART-Official <richardvanthof@pm.me>
2018-09-08 21:33:35 +0000

    fixed bug in app.py
    Fixed some made syntax errors that prevented this script from running.

--- printer/app.py
+++ printer/app.py
@@ -1,42 +1,29 @@
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, request
 import datetime
-import json
-from escpos.printer import Usb, Dummy
+from escpos.printer import Usb
 
 app = Flask(__name__)
-# p = Usb(0x0416, 0x5011)
-d = Dummy()
+p = Usb(0x0416, 0x5011)
 
 app.debug = True
-  
-}
-@app.route("/")
-def new():
-    return "Welcome to the print server!"
 
-@app.route("/test")
+@app.route("/")
 def new():
     return render_template('home.html')
 
-@app.route("/status")
-def status():
-    return render_template('status.html')
-
 @app.route("/print")
-
 def print_server():
-    
     #GET DATA FROM WEBHOOKS
     content = request.args
 
-    timestamp = datetime.datetime.now()
+    timestamp = datetime.datetime.now().timestamp()
     name = str(content.get('name'))
     phone = str(content.get('phone'))
     email = str(content.get('email'))
     message = str(content.get('''message'''))
 
     # body = "PARSED DATA: name: %s, contact: %s, message: %s" % (name, contact, message)
-    d.text("""
+    p.text("""
 
 
 IMPORTANT MESSAGE:
@@ -56,3 +43,5 @@ Phone
 _______________________________
 ===============================
 """% (timestamp, name, message, email, phone)) 
+
+    return render_template('sucess.html')
