b9a378f856a31dd20cc8681a2dac971914eea4a1
Richard van 't Hof <richardvanthof@pm.me>
2019-05-25 18:21:46 +0200

    Added 'reset printed status' function and automatic message downloading and printing funtionality
    Now the printer can automaticially download and print messages that where not printed before. It also updates the status of these messages to 'printed' and the que is cleaned up. To make development easier I added a 'reset printed status to false' function to make development easier. Also small visual changes were made to the debugging interface.

--- printer/app.py
+++ printer/app.py
@@ -46,13 +46,13 @@ def init_printer():
         return Dummy()
 
 p = init_printer()
-
+db = firestore.Client(credentials=credentials)
 
 app.debug = True
 
 def get_data():
-    db = firestore.Client(credentials=credentials)
-    db_ref = db.collection(u'Users').document(u'Richard').collection(u'Messages').where(u'printed', u'==', False)
+
+    db_ref = db.collection(u'Users').document(u'Richard').collection(u'Messages').where(u'printed', u'==', False).order_by(u'timestamp')
 
     try:
         docs = db_ref.get()
@@ -70,31 +70,74 @@ def print_message(data):
     message = str(data.get('''message'''))
 
     # body = "PARSED DATA: name: %s, contact: %s, message: %s" % (name, contact, message)
-    p.text("""
+    try:
+        p.text("""
 
-IMPORTANT MESSAGE:
-%s
+        IMPORTANT MESSAGE:
+        %s
 
-By: %s
+        By: %s
 
-%s
+        %s
 
 
-email:
-%s
+        Contact:
+        %s
 
-Phone
-%s
+        _______________________________
+        ===============================
+        """% (timestamp, name, message, contact))
+        return True
+    except:
+        return False
 
-_______________________________
-===============================
-"""% (timestamp, name, message, email, phone))
+def mark_message_as_done(message):
+    messageID = message[0]
+    messageContent = message[1]
+    print("Marking message "+str(messageID)+" as done and deleting it from print que")
+    if(update_firebase_message(messageID)):
+        if(delete_from_printque(message)):
+            return True
+        else:
+            return "Removing message from print que failed"
+    else:
+        print("Updating Firebase Failed")
+        return "Updating Firebase Failed"
+
+def update_firebase_message(messageID):
+    try:
+        doc_ref = db.collection(u'Users').document(u'Richard').collection(u'Messages').document(messageID)
+        doc_ref.update({
+            'printed': True
+        })
+        return True
+    except:
+        return "Google Cloud could not be reached"
+
+def delete_from_printque(message):
+    try:
+        message_index = messages.index(message)
+        messages.remove(messages[message_index])
+        print("Message removed");
+        return True
+    except:
+        print("Removing message from print que failed")
+        return False
 
 @app.route("/")
 def new():
     return '''
-        <h3 style="line-height: 0.9em; font-size: 3rem">Sucessfully connected to the Print server!</h3>
-        <p>Part of the Richard Hotline project by Richard van 't Hof</p>
+        <div style="padding: 5em; max-width: 75em">
+        <h1 style="color: rgba(0,0,0,0.8); font-size: 10em; line-height: 0.66em">Sucessfully connected to the Print server!</h1>
+        <h2>Part of the Richard Hotline project by Richard van 't Hof</h2>
+        <ul>
+            <li><a href="/settings">Settings</a></li>
+            <li><a href="/status">View Print que</a></li>
+            <li><a href="/reset">Reset 'printed' status</a></li>
+            <li><a href="/api/getmessages">Test Print</a></li>
+            <li><a href="/getmessages">Continuous test print</a></li>
+        </ul>
+        </div>
     '''
 
 @app.route("/status")
@@ -102,22 +145,27 @@ def display_status():
     table = "no documents"
     get_data()
     table = """
+        <h1>Print Que</h1>
         <table style="width: 100%; text-align: left">
             <tr>
-                <th>ID</th>
-                <th>From</th>
-                <th>Contact</th>
-                <th>Timestamp</th>
-                <th>Message</th>
+
+                <th><h3>ID</h3></th>
+                <th><h3>From</h3></th>
+                <th><h3>Contact</h3></th>
+                <th><h3>Timestamp</h3></th>
+                <th><h3>Message</h3></th>
+
             </tr>
             """+get_rows(messages)+"""
         </table>
     """
     return table
 
+
+
 def get_rows(posts):
     rows = []
-    table = ""
+    table = " "
 
     for message in posts:
         rows.append("""
@@ -137,7 +185,38 @@ def get_rows(posts):
 
             )
         )
-    return(" ".join(rows))
+    table = " ".join(rows)
+    return(table)
+
+@app.route("/api/getmessages")
+def manually_handle_incomming_message():
+    get_data()
+    for message in messages:
+        print_message(message[1])
+    return "connected"
+
+@app.route("/settings")
+def settings():
+    return "Feature not yet available"
+
+@app.route("/getmessages")
+def handle_incomming_messages():
+    get_data()
+    p = init_printer()
+    while(messages <= 0):
+        handle_incomming_messages()
+
+    if(messages > 0):
+        for message in messages:
+            if(print_message(message[1])):
+                mark_message_as_done(message)
+            else:
+                print("Error, message "+str(message[0])+" can't be printed. Printer seems unreachable. Please check cable.")
+                return handle_incomming_messages()
+        return "printing"
+
+
+
 
 @app.route("/api/print",  methods=['POST'])
 def post_messages():
@@ -146,6 +225,35 @@ def post_messages():
     content = request.get_json()
     print_message(content)
     return "Message sent", 200
+
+
+@app.route("/reset")
+# Resets the 'printed' status of all messages to False; should only be used for debugging
+def reset_printstatus():
+    db_ref = db.collection(u'Users').document(u'Richard').collection(u'Messages').where(u'printed', u'==', True)
+    allPosts = []
+    try:
+        docs = db_ref.get()
+        for doc in docs:
+            allPosts.append([doc.id, doc.to_dict()])
+    except google.cloud.exceptions.NotFound:
+        print 'No such document found'
+        return "failed"
+
+    try:
+        for post in allPosts:
+            doc_ref = db_ref.document(post[0])
+            doc_ref.update({
+                'printed': False
+            })
+        return """
+            <h1>Printed status reset</h1>
+            <p>The 'printed' status of all messages have been reset to 'False.'</p>
+            <a href="/status">[status]</a>
+            <a href="/getmessages">[test printer]</a>
+        """
+    except:
+        return EnvironmentError
 
 
 if __name__ == '__main__':
